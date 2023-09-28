25786f047b51a098e1fbceb090809e809049b7d3
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-09-28 20:55:05 -0300

    create errors

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import Product from './product.model';
 
 @Injectable()
@@ -9,34 +9,50 @@ export class ProductService {
     new Product(3, 'P003', 'Product 3', 20.99),
   ];
 
+  private throwProductNotFoundException(): never {
+    throw new NotFoundException('Product not found.');
+  }
+
   findAll() {
     return this.products;
   }
 
-  findById(id: number) {
-    return this.products.find((product) => product.id === id);
+  findById(id: number): Product {
+    const product = this.products.find((product) => product.id === id);
+    if (!product) {
+      this.throwProductNotFoundException();
+    }
+    return product;
   }
 
-  create(product: Product) {
+  create(product: Product): Product {
     this.products.push(product);
     return product;
   }
 
-  update(id: number, product: Product) {
-    //this.products[id] = product;
+  update(id: number, product: Product): Product {
+    const productIndex = this.products.findIndex((prod) => prod.id === id);
+
+    if (productIndex === -1) {
+      this.throwProductNotFoundException();
+    }
+
+    product.id = id;
+    this.products[productIndex] = product;
+
     return product;
   }
 
-  remove(id: number) {
+  remove(id: number): boolean {
     const indexToRemove = this.products.findIndex(
       (product) => product.id === id,
     );
 
-    if (indexToRemove !== -1) {
-      this.products.splice(indexToRemove, 1);
-      return true; // Indica que a remoção foi bem-sucedida
+    if (indexToRemove === -1) {
+      this.throwProductNotFoundException();
     }
 
-    return false; // Indica que o produto com o ID especificado não foi encontrado
+    this.products.splice(indexToRemove, 1);
+    return true;
   }
 }
